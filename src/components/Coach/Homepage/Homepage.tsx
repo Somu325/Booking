9cde9ab0aa25e1dcc05816b8e02fc74034decb6b
jagadish22002 <156312491+jagadish22002@users.noreply.github.com>
2024-10-31 18:00:46 +0530

    Update Homepage.tsx

--- src/components/Coach/Homepage/Homepage.tsx
+++ src/components/Coach/Homepage/Homepage.tsx
@@ -566,7 +566,7 @@ const Coachdashboard: React.FC = () => {
       setError(null);
     } catch (err: any) {
       console.error('Error fetching slots:', err.message);
-      setError('Failed to load slots. Please try again later.');
+      setError('No slots are available');
       setSlots([]);
     } finally {
       setLoading(false);
